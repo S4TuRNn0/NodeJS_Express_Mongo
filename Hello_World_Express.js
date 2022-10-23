e57717b8ccd5e20862cc01744a65aee00b806600
Angie_Echeverry <judiithecheverry24@gmail.com>
2022-10-23 00:19:23 -0500

    Reconfiguring changes in Express branch

--- Hello_World_Express.js
+++ Hello_World_Express.js
@@ -1,11 +1,11 @@
-const espress = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World from express!')
-})
+    res.send('Hello World from express!');
+});
 
 app.listen(port, () => {
-    console.log('Example app listening on port ${port}')
-})
+    console.log(`Example app listening on port ${port}`);
+});
