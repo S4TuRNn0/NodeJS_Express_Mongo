5c6aba82af6f9fad2012deb8a82f3e9183b967bd
Angie_Echeverry <judiithecheverry24@gmail.com>
2022-10-25 15:17:23 -0500

    Adding the User model

---
+++ Models/Usuario_Model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const usuarioSchema = new mongoose.Schema({
+    email:{
+        type:String,
+        required: true
+    },
+    nombre: {
+        type:String,
+        required: true
+    },
+    password: {
+        type:String,
+        required: true
+    },
+    estado: {
+        type:Boolean,
+        default: true
+    },
+    image: {
+        type:String,
+        required: false
+    }
+});
+
+module.exports = mongoose.model('Usuarios', usuarioSchema);
