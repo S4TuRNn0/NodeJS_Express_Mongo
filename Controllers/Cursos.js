9fdfd536e6cda63f812180d1c800824e3739b46f
Angie_Echeverry <judiithecheverry24@gmail.com>
2022-10-28 16:57:50 -0500

    Adding and Testing POST Type Endpoint Functionality for Course Creation

--- Controllers/Cursos.js
+++ Controllers/Cursos.js
@@ -8,3 +8,27 @@ ruta.get('/', (req,res)=>{
 
 
 module.exports = ruta;
+//Endpoint Tipo POST Para Recurso CURSOS
+ruta.post('/',(req, res)=>{
+    let resultado = crearCurso(req.body);
+
+    resultado.then(curso =>{
+        res.json({
+            curso
+        })
+    }).catch(err => {
+        res.status(400).json({
+            err
+        })
+    })
+});
+
+async function crearCurso(body){
+    let curso = new Curso({
+        titulo  :body.titulo,
+        descripcion :body.descripcion,
+        alumnos :body.alumnos,
+        calificacion    :body.calificacion
+    });
+    return await curso.save();
+}
