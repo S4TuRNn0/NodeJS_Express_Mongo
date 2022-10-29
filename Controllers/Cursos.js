08ebea898d3bcf2c3b502bbeb82412d193a86cb7
Angie_Echeverry <judiithecheverry24@gmail.com>
2022-10-28 22:02:44 -0500

    Adding and Testing PUT Type Endpoint Functionality for Course Edit

--- Controllers/Cursos.js
+++ Controllers/Cursos.js
@@ -32,3 +32,25 @@ async function crearCurso(body){
     });
     return await curso.save();
 }
+// Funcion Asincrona Para Actualizar CURSOS
+async function actualizarCurso(id, body){
+    let curso = await Curso.findByIdAndUpdate(id, {
+        $set:{
+            titulo: body.titulo,
+            descripcion: body.descripcion
+        }
+    }, {new: true});
+    return curso;
+}
+//Endpoint Tipo PUT Para El Recurso CURSOS
+ruta.put('/:id', (req, res) =>{
+    let resultado = actualizarCurso(req.params.id, req.body);
+    resultado.then(curso => {
+        res.json(curso)
+    }).catch(err =>{
+        res.status(400).json(err)
+    })
+})
+
+
+
